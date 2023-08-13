31d8159e0cb19181156eb8bd11b64113f350372e
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2023-08-13 14:52:55 +0530

    style: detail page

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,11 +1,14 @@
 import { useParams } from "react-router-dom";
 import { useMoviesContext } from "../context/MoviesContext";
+import "../styles/home.css";
 
 export default function MovieDetail() {
-  const { moviesState } = useMoviesContext();
+  const { moviesState, moviesDispatch } = useMoviesContext();
   const { movieId } = useParams();
 
   const movie = moviesState.movies.find(({ id }) => id === Number(movieId));
+  const isStarred = moviesState.starredMovies.find(({ id }) => id === movie.id);
+  const isWatchList = moviesState.watchList.find(({ id }) => id === movie.id);
   const {
     title,
     summary,
@@ -19,7 +22,7 @@ export default function MovieDetail() {
   } = movie;
 
   return (
-    <div className="container-fluid text-center">
+    <div className="container-fluid text-center  detail-main">
       <div class="card mb-3" style={{ maxWidth: "700px" }}>
         <div class="row no-gutters">
           <div class="col-md-4 ">
@@ -40,13 +43,41 @@ export default function MovieDetail() {
               <p class="card-text">Director : {director}</p>
               <p class="card-text">Writer: {writer}</p>
               <p class="card-text">Cast : {cast}</p>
-              <div className="action-btn">
-                {" "}
-                <button type="button" class="btn btn-dark">
-                  Star
+              <div className="action-btn-detail">
+                <button
+                  type="button"
+                  class="btn btn-dark"
+                  onClick={() => {
+                    if (isStarred) {
+                      moviesDispatch({
+                        type: "REMOVE_FROM_STARRED",
+                        movie: movie,
+                      });
+                    } else {
+                      moviesDispatch({ type: "ADD_TO_STARRED", movie: movie });
+                    }
+                  }}
+                >
+                  {isStarred ? "unstar" : "star"}
                 </button>
-                <button type="button" class="btn btn-dark">
-                  Add
+                <button
+                  type="button"
+                  class="btn btn-dark"
+                  onClick={() => {
+                    if (isWatchList) {
+                      moviesDispatch({
+                        type: "REMOVE_FROM_WATCHLIST",
+                        movie: movie,
+                      });
+                    } else {
+                      moviesDispatch({
+                        type: "ADD_TO_WATCHLIST",
+                        movie: movie,
+                      });
+                    }
+                  }}
+                >
+                  {isWatchList ? "Remove from  watchlist" : "Add to  watchlist"}
                 </button>
               </div>
             </div>
